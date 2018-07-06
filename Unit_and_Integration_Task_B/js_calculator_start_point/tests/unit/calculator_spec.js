412919f016994f99fcf36b9d4dfbc81a5cc09c77
elipinska <ewa.lipinska@hotmail.com>
2018-07-06 09:20:14 +0100

    Add second testing exercise files

---
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -0,0 +1,14 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+});
